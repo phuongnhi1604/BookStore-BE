51084c2dde18f7ed2d2e57e4cf7722711240ed36
phuongnhi1604 <phuongnhi1641@gmail.com>
2022-11-17 12:00:55 +0700

    push search

--- BookStore-BE/src/main/java/module6/backend/bookstorebe/repository/BookRepository.java
+++ BookStore-BE/src/main/java/module6/backend/bookstorebe/repository/BookRepository.java
@@ -26,13 +26,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query(value = "SELECT * FROM book where book_author_id=?1 order by book_publish_date desc limit 4", nativeQuery = true)
     List<Book> findAllBookByAuthorId(Long book_author_id);
 
-    @Query(value = "SELECT * FROM book left join author on author.author_id = book.book_author_id where book.book_flag = 0 and book.book_author_id=?1  order by book.book_author_id limit 0,4", nativeQuery = true)
+    @Query(value = "SELECT * FROM book left join author on author.author_id = book.book_author_id where book.book_flag = 0 and book.book_author_id=?1 order by book.book_author_id limit 0,4", nativeQuery = true)
     List<Book>findBookSameAuthor(Long book_author_id);
 
     @Query(value = "SELECT * FROM book where book_promotion_id  <> 1", nativeQuery = true)
     Page<Book> findAllBookByPromotion(Pageable pageable);
 
-    @Query(value = "SELECT * FROM book b join category c on b.book_category_id = c.category_id join author au on b.book_author_id = au.author_id where b.book_name like concat('%',:search_key,'%') or c.category_name like concat('%',:search_key,'%')  or au.author_name like concat('%',:search_key,'%')", nativeQuery = true,
-            countQuery = "SELECT count(*) from (SELECT * FROM book b join category c on b.book_category_id = c.category_id join author au on b.book_author_id = au.author_id where b.book_name like concat('%',:search_key,'%') or c.category_name like concat('%',:search_key,'%')  or au.author_name like concat('%',:search_key,'%')) abc ")
+    @Query(value = "SELECT * FROM book b join category c on b.book_category_id = c.category_id join author au on b.book_author_id = au.author_id where b.book_name like concat('%',:search_key,'%') or c.category_name like concat('%',:search_key,'%')  or au.author_name like concat('%',:search_key,'%') order by b.book_publish_date desc", nativeQuery = true,
+            countQuery = "SELECT count(*) from (SELECT * FROM book b join category c on b.book_category_id = c.category_id join author au on b.book_author_id = au.author_id where b.book_name like concat('%',:search_key,'%') or c.category_name like concat('%',:search_key,'%')  or au.author_name like concat('%',:search_key,'%')  order by b.book_publish_date desc) abc ")
     Page<Book> searchBook(@Param("search_key") String search_key, Pageable pageable);
 }
